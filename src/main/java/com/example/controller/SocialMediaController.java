69f5d833a4b7b867146a8f89a8f30597e2969ff7
Colin McMurtray <mcmurtraycolin@gmail.com>
2023-10-05 21:16:14 +0000

    Refactored Javilin code from old project SocialMediaController.java file and modified to AccountRepository to extend JpaRepository

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import com.example.entity.Account;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +17,57 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+@RestController
+@RequestMapping("/api") // Define a base URL for all endpoints
 public class SocialMediaController {
 
+    private final AccountService accountService;
+    private final MessageService messageService;
+
+    @Autowired // Inject dependencies using Spring's DI
+    public SocialMediaController(AccountService accountService, MessageService messageService) {
+        this.accountService = accountService;
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/register") // Handle POST requests at /api/register
+    public Account registerUser(@RequestBody Account account) throws JsonProcessingException {
+        return accountService.registerUser(account);
+    }
+
+    @PostMapping("/login") // Handle POST requests at /api/login
+    public Account loginUser(@RequestBody Account account) throws JsonProcessingException {
+        return accountService.authenticateUser(account);
+    }
+
+    @PostMapping("/messages") // Handle POST requests at /api/messages
+    public Message createMessage(@RequestBody Message message) throws JsonProcessingException {
+        return messageService.createMessage(message);
+    }
+
+    @GetMapping("/messages") // Handle GET requests at /api/messages
+    public List<Message> getAllMessages() {
+        return messageService.getAllMessages();
+    }
+
+    @GetMapping("/messages/{message_id}") // Handle GET requests at /api/messages/{message_id}
+    public Message getMessageById(@PathVariable int message_id) {
+        return messageService.getMessageById(message_id);
+    }
+
+    @DeleteMapping("/messages/{message_id}") // Handle DELETE requests at /api/messages/{message_id}
+    public Message deleteMessageById(@PathVariable int message_id) {
+        return messageService.deleteMessageById(message_id);
+    }
+
+    @PutMapping("/messages/{message_id}") // Handle PUT requests at /api/messages/{message_id}
+    public Message updateMessage(@PathVariable int message_id, @RequestBody Message message) {
+        // Your update logic here
+    }
+
+    @GetMapping("/accounts/{account_id}/messages") // Handle GET requests at /api/accounts/{account_id}/messages
+    public List<Message> getMessagesByUser(@PathVariable int account_id) {
+        return messageService.getMessagesByUser(account_id);
+    }
 }
