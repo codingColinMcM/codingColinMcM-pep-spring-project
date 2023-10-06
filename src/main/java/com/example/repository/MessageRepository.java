360dc1a94fbcca9c2b9264d6160e5a801a600bfc
Colin McMurtray <mcmurtraycolin@gmail.com>
2023-10-06 01:34:24 +0000

    Ready for testing

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -3,5 +3,10 @@ package com.example.repository;
 import com.example.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface MessageRepository extends JpaRepository<Message, Long>{
+
+    List<Message> findByPostedBy(Long userID);
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,50 @@
 package com.example.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.example.repository.MessageRepository;
+import com.example.entity.Message;
 
+@Service
 public class MessageService {
+    private final MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public Message createMessage(Message message) {
+        return messageRepository.save(message);
+    }
+
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    public Message getMessageById(Long messageId) {
+        return messageRepository.findById(messageId).orElse(null);
+    }
+
+    public Message deleteMessageById(Long messageId) {
+        Message message = getMessageById(messageId);
+        if (message != null) {
+            messageRepository.delete(message);
+        }
+        return message;
+    }
+
+    public Message updateMessage(Long messageId, Message updatedMessage) {
+        Message existingMessage = getMessageById(messageId);
+        if (existingMessage != null) {
+            existingMessage.setMessage_text(updatedMessage.getMessage_text());
+            existingMessage.setTime_posted_epoch(updatedMessage.getTime_posted_epoch());
+            messageRepository.save(existingMessage);
+        }
+        return existingMessage;
+    }
+
+    public List<Message> getMessagesByUser(Long postedBy) {
+        return messageRepository.findByPostedBy(postedBy);
+    }
 }
