fe3a16ecf90f7907bc83d1d962bf08f1bb0dea18
Colin McMurtray <mcmurtraycolin@gmail.com>
2023-10-10 17:49:24 +0000

    commit to save latest work

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -31,43 +31,42 @@ public class SocialMediaController {
         this.messageService = messageService;
     }
 
-    @PostMapping("/register") // Handle POST requests at /api/register
+    @PostMapping("/register") // Handle POST requests at /register
     public Account registerUser(@RequestBody Account account) throws JsonProcessingException {
         return accountService.registerUser(account);
     }
 
-    @PostMapping("/login") // Handle POST requests at /api/login
+    @PostMapping("/login") // Handle POST requests at /login
     public Account loginUser(@RequestBody Account account) throws JsonProcessingException {
         return accountService.authenticateUser(account.getUsername(), account.getPassword());
     }
 
-    @PostMapping("/messages") // Handle POST requests at /api/messages
+    @PostMapping("/messages") // Handle POST requests at /messages
     public Message createMessage(@RequestBody Message message) throws JsonProcessingException {
         return messageService.createMessage(message);
     }
 
-    @GetMapping("/messages") // Handle GET requests at /api/messages
+    @GetMapping("/messages") // Handle GET requests at /messages
     public List<Message> getAllMessages() {
         return messageService.getAllMessages();
     }
 
-    @GetMapping("/messages/{message_id}") // Handle GET requests at /api/messages/{message_id}
+    @GetMapping("/messages/{message_id}") // Handle GET requests at /messages/{message_id}
     public Message getMessageById(@PathVariable int message_id) {
         return messageService.getMessageById(message_id);
     }
 
-    @DeleteMapping("/messages/{message_id}") // Handle DELETE requests at /api/messages/{message_id}
+    @DeleteMapping("/messages/{message_id}") // Handle DELETE requests at /messages/{message_id}
     public Message deleteMessageById(@PathVariable int message_id) {
         return messageService.deleteMessageById(message_id);
     }
 
-    @PutMapping("/messages/{message_id}") // Handle PUT requests at /api/messages/{message_id}
+    @PatchMapping("/messages/{message_id}") // Handle PUT requests at /messages/{message_id}
     public Message updateMessage(@PathVariable int message_id, @RequestBody Message message) {
-        // Your update logic here
         return messageService.updateMessage(message_id, message);
     }
 
-    @GetMapping("/accounts/{account_id}/messages") // Handle GET requests at /api/accounts/{account_id}/messages
+    @GetMapping("/accounts/{account_id}/messages") // Handle GET requests at /accounts/{account_id}/messages
     public List<Message> getMessagesByUser(@PathVariable int account_id) {
         return messageService.getMessagesByUser(account_id);
     }
