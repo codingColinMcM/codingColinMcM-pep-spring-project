32af9d44f2acd9f0edbeada3c94c7b405fa49a3e
Colin McMurtray <mcmurtraycolin@gmail.com>
2023-10-12 17:21:23 +0000

    Commented AccountService.java

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -4,7 +4,6 @@ import com.example.entity.Account;
 import com.example.entity.Message;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -3,10 +3,11 @@ package com.example.service;
 import com.example.entity.Account;
 import com.example.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity; // Spring's ResponseEntity for HTTP responses
 import org.springframework.stereotype.Service;
+
 import java.util.Optional;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus; // Spring's HttpStatus for HTTP status codes
 
 @Service
 public class AccountService {
@@ -18,24 +19,34 @@ public class AccountService {
         this.accountRepository = accountRepository;
     }
 
-    // Method to register a new user
+    /**
+     * Register a new user.
+     *
+     * @param account The account to register.
+     * @return ResponseEntity with the registered account if registration is successful, or a conflict response if the user already exists.
+     */
     public ResponseEntity<Object> registerUser(Account account) {
-        // Save the user using the repository
+        // Check if the user already exists by username and password
         Optional<Account> possibleUser = accountRepository.findByUsernameAndPassword(account.getUsername(), account.getPassword());
-        if (!possibleUser.isPresent()){
+        if (!possibleUser.isPresent()) {
             return ResponseEntity.ok(accountRepository.save(account));
         }
-        return new ResponseEntity<>(HttpStatus.CONFLICT);
+        return new ResponseEntity<>(HttpStatus.CONFLICT); // Conflict response if the user already exists
     }
 
-    // Method to authenticate a user
+    /**
+     * Authenticate a user by username and password.
+     *
+     * @param username The username to authenticate.
+     * @param password The password to authenticate.
+     * @return ResponseEntity with the authenticated user if successful, or an unauthorized response if authentication fails.
+     */
     public ResponseEntity<Object> authenticateUser(String username, String password) {
         Optional<Account> possibleUser = accountRepository.findByUsernameAndPassword(username, password);
-        if (possibleUser.isPresent()){
+        if (possibleUser.isPresent()) {
             Account realUser = possibleUser.get();
             return ResponseEntity.ok(realUser);
         }
-        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); // Unauthorized response if authentication fails
     }
 }
-
