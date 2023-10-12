814e381d4283b66b743713298614007077af1787
Colin McMurtray <mcmurtraycolin@gmail.com>
2023-10-12 16:41:07 +0000

    Added comments to AccountRepository

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,15 +1,27 @@
 package com.example.repository;
 
-import com.example.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.Optional;
+import org.springframework.stereotype.Repository; // Spring Framework's repository annotation
+import org.springframework.data.jpa.repository.JpaRepository; // Spring Data JPA's repository interface
+import java.util.Optional; // Java's Optional class for representing possibly null values
 
+import com.example.entity.Account; // Import custom Account entity
+
+/**
+ * Repository interface for managing Account entities.
+ *
+ * This interface extends JpaRepository, which provides standard CRUD operations
+ * for the Account entity. Additional custom query methods can be defined here.
+ */
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
 
-    // Custom query method to find an account by username and password
+    /**
+     * Custom query method to find an account by username and password.
+     *
+     * @param username The username of the account.
+     * @param password The password of the account.
+     * @return An Optional containing the Account if found, or an empty Optional if not found.
+     */
     Optional<Account> findByUsernameAndPassword(String username, String password);
-
 }
 
